050410453a11f88e5f9b5690bf40e1a057e19318
Pasot01 <contactmyphone14@gmail.com>
2022-08-05 19:36:38 +0200

    Configuration du formulaire la page Signup et mise en place de la gestion des évènements

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, Button, TextField, Typography } from '@mui/material';
 
 const Signup = () => {
+    const [inputs, setInputs] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+    const handleChange = (e) => {
+        setInputs((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+        console.log(e.target.name, 'value', e.target.value);
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(inputs);
+    };
     return (
-        <div>Signup</div>
+        <div>
+
+            <form onSubmit={handleSubmit}>
+                <Box
+                    marginLeft='auto'
+                    marginRight='auto'
+                    width={300}
+                    display='flex'
+                    flexDirection={'column'}
+                    justifyContent='center'
+                    alignItems='center'
+                >
+                    <Typography variant='h2'>Signup</Typography>
+                    <TextField
+                        name="name"
+                        onChange={handleChange}
+                        value={inputs.name}
+                        variant="outlined"
+                        placeholder="Name"
+                        margin="normal"
+                    />
+                    <TextField
+                        name="email"
+                        onChange={handleChange}
+                        type={"email"}
+                        value={inputs.email}
+                        variant="outlined"
+                        placeholder="Email"
+                        margin="normal"
+                    />
+                    <TextField
+                        name="password"
+                        onChange={handleChange}
+                        type="password"
+                        value={inputs.password}
+                        variant="outlined"
+                        placeholder="Password"
+                        margin="normal"
+                    />
+                    <Button variant='contained' type='submit'>
+                        Signup
+                    </Button>
+                </Box>
+            </form>
+        </div>
+
     )
 }
 
